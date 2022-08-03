0c8fdfd1ad79898ff8b9dc26b0f7d8aa1865725d
FunnyVodi4ka <a_zuev@devspark.ru>
2022-08-03 17:55:35 +0300

    Create mirgations for users, courses and assignments

---
+++ database/migrations/2022_08_03_142148_create_courses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id('course_id');
+            $table->string('title', 255);
+            $table->foreignId('author_id')
+                ->references('user_id')
+                ->on('users');
+            $table->string('description', 255)->nullable();
+            $table->json('content')->nullable();
+            $table->timestamps();
+            $table->softDeletes()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+};
---
+++ database/migrations/2022_08_03_143605_create_assignments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assignments', function (Blueprint $table) {
+            $table->id('assignment_id');
+            $table->foreignId('student_id')
+                ->references('user_id')
+                ->on('users');
+            $table->foreignId('course_id')
+                ->references('course_id')
+                ->on('courses');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assignments');
+    }
+};
