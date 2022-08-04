8bdfcff47031b68f9bc6a10aba1a4c63a637f694
Mezuno <mekishido@gmail.com>
2022-08-04 17:30:43 +0300

    Create profile view
    Create profile view (users show)
    Small css and views fixes

--- resources/views/components/aside.blade.php
+++ resources/views/components/aside.blade.php
@@ -1,14 +1,13 @@
 <aside class="aside">
     <div class="aside__header">
         <div class="aside__logo logo h2">CourseZone</div>
-        <a href="" class="aside__link button">Users</a>
-        <a href="" class="aside__link button">Courses</a>
+        <a href="{{ url('/users') }}" class="aside__link button">Students</a>
+        <a href="" class="aside__link button">Assigned courses</a>
         <a href="" class="aside__link button">My courses</a>
-        <a href="" class="aside__link button">Students</a>
     </div>
 
     <div class="aside__footer">
-        <a href="" class="aside__link aside__link_profile">mail@email.com</a>
+        <a href="{{ url('/users/'.'1') }}" class="aside__link aside__link_profile">mail@email.com</a>
         <a href="{{ url('/login') }}" class="aside__link button">Logout</a>
     </div>
 </aside>
