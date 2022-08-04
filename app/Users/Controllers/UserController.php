404d6423c1f21508629e04d90ba28da7b30dec5e
forest-chan <forest-chan@mail.ru>
2022-08-04 11:55:13 +0300

    added return view() to the actions

--- app/Users/Controllers/UserController.php
+++ app/Users/Controllers/UserController.php
@@ -13,22 +13,37 @@ class UserController extends Controller
 
     public function index(Request $request)
     {
+        return view('users');
     }
 
     public function show(Request $request)
     {
+        return view();
     }
 
     public function edit(Request $request)
     {
+        return view();
     }
 
     public function update(Request $request)
     {
+        return view();
     }
 
     public function destroy(Request $request)
     {
+
+    }
+
+    public function login(Request $request)
+    {
+        return view('login');
+    }
+
+    public function register(Request $request)
+    {
+        return view('register');
     }
 
 }
