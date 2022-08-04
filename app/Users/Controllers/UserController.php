46f6f77f0c739814f3ffe3001a9cbb8c1799148c
forest-chan <forest-chan@mail.ru>
2022-08-04 12:48:00 +0300

    added courses controller and fixed undefined behaviour

--- app/Users/Controllers/UserController.php
+++ app/Users/Controllers/UserController.php
@@ -8,6 +8,8 @@ use Illuminate\View\View;
 
 class UserController extends Controller
 {
+    private $pathToView = '../Views/';
+
     public function __construct()
     {
     }
@@ -46,14 +48,6 @@ class UserController extends Controller
 
     }
 
-    public function login(Request $request)
-    {
-        return view('login');
-    }
 
-    public function register(Request $request)
-    {
-        return view('register');
-    }
 
 }
