3ff6d1d077fadae376789a2706e5d37ea9ae4a38
FunnyVodi4ka <a_zuev@devspark.ru>
2022-08-05 17:51:32 +0300

    fix title and description of courses

--- database/factories/CourseFactory.php
+++ database/factories/CourseFactory.php
@@ -20,7 +20,7 @@ class CourseFactory extends Factory
     {
         return [
             'title' => fake()->company(),
-            'author_id' => rand(1, 20), //пользователей созданные фабрикой пользователи
+            'author_id' => 1, //rand(1, *максимальный id пользователя*) количество пользователей созданные фабрикой
             'description' => fake()->text(255),
             'content' => '{"0": {"type": "text", "content": "text"},
             "1": {"type": "link", "content": "https://www.youtube.com/"}}',
