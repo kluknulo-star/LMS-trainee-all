510179ddf56cecf798e82250695622a23f484a10
kirill <study.klkv@mail.ru>
2022-08-10 11:54:40 +0300

    Add Validation for Course
    Create CourseCreateRequest and UpdateCourseRequest.php

---
+++ app/Http/Requests/CreateCourseRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateCourseRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required|unique:courses,title|regex:~^([^<>]*)$~|between:10,90',
+            'description' => 'nullable|regex:~^([^<>]*)$~|max:255',
+        ];
+    }
+}
---
+++ app/Http/Requests/UpdateCourseRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateCourseRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'title' => [
+                'required',
+                Rule::unique('courses')->ignore($this->route('id'), 'course_id'),
+                'regex:~^([^<>]*)$~',
+                'between:10,90',
+                ],
+            'description' => 'nullable|regex:~^([^<>]*)$~|max:255',
+        ];
+    }
+}
