15f6c3fed54984f8bbba38b1b4dce2246d5ace92
forest-chan <forest-chan@mail.ru>
2022-08-11 17:01:49 +0300

    CourseContentController added

---
+++ app/Courses/Controllers/CourseContentController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Courses\Controllers;
+
+use App\Courses\Services\CourseContentService;
+use App\Http\Controllers\Controller;
+
+class CourseContentController extends Controller
+{
+    public function __construct(private CourseContentService $service)
+    {
+
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function play()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
+
+}
