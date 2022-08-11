6d0ac85d655578840eb31a6f9d62cad2f9edc69f
FunnyVodi4ka <a_zuev@devspark.ru>
2022-08-11 10:56:22 +0300

    fix errors WITH images

---
+++ resources/views/pages/courses/play.blade.php
@@ -0,0 +1,12 @@
+@component('components.header')
+@endcomponent
+
+@component('components.aside')
+@endcomponent
+
+<div class="container">
+    dfsdfsdf
+</div>
+
+@component('components.footer')
+@endcomponent
