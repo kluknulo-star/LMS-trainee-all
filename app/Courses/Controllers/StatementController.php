4eb8a9605efa56c2c9344cf936803b31ce50d5fb
kirill <study.klkv@mail.ru>
2022-08-17 16:51:35 +0300

    Get and send statements LRS
    Create class ClientLRS and other things

---
+++ app/Courses/Controllers/StatementController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Courses\Controllers;
+
+use App\Courses\Helpers\ClientLRS;
+use App\Courses\Services\CourseService;
+
+
+use App\Http\Controllers\Controller;
+use App\Users\Models\User;
+use Illuminate\Http\Client\Response;
+use Illuminate\Http\Request;
+
+
+class StatementController extends Controller
+{
+    public function __construct(private CourseService $courseService)
+    {
+
+    }
+
+    public function sendLaunchCourseStatement(Request $request, int $courseId, int $sectionId) : Response
+    {
+        $course = $this->courseService->getCourse($courseId);
+        $course->content = json_decode($course->content);
+        $section = [];
+
+        foreach ($course->content as $element) {
+            if ($element->section_id == $sectionId) {
+                $section = $element;
+                break;
+            }
+        }
+
+        /** @var User $user */
+        $user = auth()->user();
+
+        $statement = ClientLRS::compileStatement($user, 'launched', $course, $section);
+
+        return ClientLRS::sendStatement($statement);
+    }
+
+    public function sendPassCourseStatement(Request $request, int $courseId, int $sectionId) : Response
+    {
+        $course = $this->courseService->getCourse($courseId);
+        $course->content = json_decode($course->content);
+        $section = [];
+
+        foreach ($course->content as $element) {
+            if ($element->section_id == $sectionId) {
+                $section = $element;
+                break;
+            }
+        }
+
+        /** @var User $user */
+        $user = auth()->user();
+
+        $statement = ClientLRS::compileStatement($user, 'passed', $course, $section);
+
+        return ClientLRS::sendStatement($statement);
+    }
+
+    public function getMyCourseProgress()
+    {
+        /** @var User $user */
+        $user = auth()->user();
+        $response = ClientLRS::getStatements($user->name);
+        $statements = json_decode($response->body(), true)["body"];
+
+//        dd($statements);
+
+//        foreach($statements as $statement) {
+//            echo $statement['id'];
+//            $statement['content'] = json_decode($statement['content']);
+//            echo "<pre>" . json_encode($statement['content'], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT) . "</pre>";
+//        }
+        return $statements;
+    }
+
+}
