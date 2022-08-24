2ad7ef5dfd65db4b864d8f671c41148149750923
FunnyVodi4ka <a_zuev@devspark.ru>
2022-08-24 11:26:41 +0300

    Create two command for transfer data between content column and course_items table

---
+++ app/Console/Commands/ContentTransfer.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Courses\Models\Course;
+use App\Courses\Models\CourseItems;
+use App\Courses\Models\TypeOfItems;
+use Illuminate\Console\Command;
+use Illuminate\Database\Query\Expression;
+use Illuminate\Support\Facades\DB;
+
+class ContentTransfer extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:contentTransfer';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Transfer data from the content column to the course_items table';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $courses = Course::withTrashed()->get();
+
+        foreach($courses as $course) {
+            $courseId = $course['course_id'];
+
+            foreach (json_decode($course['content']) as $item) {
+                $itemType = $item->type;
+                $typeOfItem = TypeOfItems::firstOrCreate(
+                    ['type' => $itemType]
+                );
+                $itemTitle = $item->title;
+                $itemContent = $item->content;
+
+                $newItem = CourseItems::create([
+                    'course_id' => $courseId,
+                    'type_id' => $typeOfItem->type_id,
+                    'title' => $itemTitle,
+                    'item_content' => json_encode($itemContent),
+                ]);
+                if($newItem) {
+                    DB::table('courses')->where('course_id', $courseId)->update(['content' => new Expression('(JSON_ARRAY())')]);
+                }
+            }
+        }
+    }
+}
---
+++ app/Console/Commands/ContentTransferBack.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Courses\Models\Course;
+use App\Courses\Models\CourseItems;
+use App\Courses\Models\TypeOfItems;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class ContentTransferBack extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:contentTransferBack';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Move the data from the course_itemstable table to the content column';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $items = CourseItems::all();
+        foreach ($items as $item) {
+            $itemId = $item->item_id;
+            $courseId = $item->course_id;
+            $itemType = TypeOfItems::where('type_id', $item->type_id)->first()->type;
+            $itemTitle = $item->title;
+            $itemContent = json_decode($item->item_content, TRUE);
+
+            $course = Course::where('course_id', $courseId)->first();
+            $content = json_decode($course->content, TRUE);
+            $newItem = [
+                'type' => $itemType,
+                'title' => $itemTitle,
+                'content' => $itemContent,
+                'section_id' => count($content),
+            ];
+            $content[] = $newItem;
+            $course->content = json_encode($content);
+            $course->save();
+            if($newItem) {
+                DB::table('course_items')->where('item_id', $itemId)->delete();
+            }
+        }
+    }
+}
