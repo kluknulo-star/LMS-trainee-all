fb9ce3d03b287109dbd3bf16371b216fc040f18b
FunnyVodi4ka <a_zuev@devspark.ru>
2022-08-30 12:56:30 +0300

    fixed quizzes options creating

--- resources/views/pages/courses/quizzes/options.blade.php
+++ resources/views/pages/courses/quizzes/options.blade.php
@@ -14,7 +14,7 @@
         <div class="form">
             <select id="options">
                 @forelse($options as $option)
-                    <option {{ $option->is_correct ? 'selected' : '' }} value='{{ $option->option_body }}'>{{ $option->option_body }}</option>
+                    <option class="quiz-option" {{ $option->is_correct ? 'selected' : '' }} value='{{ $option->option_body }}'>{{ $option->option_body }}</option>
                     @empty
                 @endforelse
             </select>
@@ -57,6 +57,7 @@
 
         if (value !== '') {
             opt = document.createElement('option');
+            opt.classList.add('quiz-option');
             opt.value = value;
             opt.innerHTML = value;
             options.appendChild(opt);
@@ -64,7 +65,8 @@
         }
     }
     function saveChanges() {
-        let options = document.querySelectorAll('option');
+        let options = document.querySelectorAll('.quiz-option');
+        console.log(options);
         let obj = [];
         options.forEach(option => {
             obj.push({
@@ -72,6 +74,7 @@
                 "isCorrect": option.selected,
             });
         });
+        console.log(obj);
         fetch("{{ route('quiz.options.store', ['id' => $id, 'section_id' => $section_id, 'quiz' => $quiz, 'question' => $question]) }}", {
             method: 'POST',
             headers: {
